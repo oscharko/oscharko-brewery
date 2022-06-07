21f2f90b60f9d626b74bbdf1ad9b8d5eca74108a
Oliver Scharkowski <o.scharkowski@oscharko.de>
2022-06-07 17:00:21 +0200

    Initial Commit

---
+++ src/main/java/oscharko/de/oscharkobrewery/OscharkoBreweryApplication.java
@@ -0,0 +1,13 @@
+package oscharko.de.oscharkobrewery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OscharkoBreweryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OscharkoBreweryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/oscharko/de/oscharkobrewery/OscharkoBreweryApplicationTests.java
@@ -0,0 +1,13 @@
+package oscharko.de.oscharkobrewery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OscharkoBreweryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
